8cfa6d466a1be03a75fd88fd9fdbc1aa29d230be
JanLubiarz <janek@andrew.home.pl>
2024-04-08 12:50:16 +0200

    Koniec zadania

--- Lab03_MultiThreadPhoto/Form1.Designer.cs
+++ Lab03_MultiThreadPhoto/Form1.Designer.cs
@@ -28,12 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.OliveDrab;
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button1.ForeColor = SystemColors.ButtonFace;
+            button1.Location = new Point(12, 348);
+            button1.Name = "button1";
+            button1.Size = new Size(106, 74);
+            button1.TabIndex = 0;
+            button1.Text = "Load";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.OliveDrab;
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button2.ForeColor = SystemColors.ButtonFace;
+            button2.Location = new Point(124, 348);
+            button2.Name = "button2";
+            button2.Size = new Size(103, 74);
+            button2.TabIndex = 1;
+            button2.Text = "Parallel processing";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(329, 302);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(401, 184);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(172, 130);
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(401, 12);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(172, 118);
+            pictureBox3.TabIndex = 4;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(600, 12);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(172, 118);
+            pictureBox4.TabIndex = 5;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(600, 184);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(172, 130);
+            pictureBox5.TabIndex = 6;
+            pictureBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkKhaki;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Shrek App";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private OpenFileDialog openFileDialog1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
     }
 }
--- Lab03_MultiThreadPhoto/Form1.cs
+++ Lab03_MultiThreadPhoto/Form1.cs
@@ -1,10 +1,177 @@
+using System.Windows.Forms;
+
 namespace Lab03_MultiThreadPhoto
 {
     public partial class Form1 : Form
     {
+        public static readonly object locker = new object();
+        private Bitmap? img;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = "C:\\Users\\Jan £ubiarz\\OneDrive\\Pobrane_assety\\Pulpit\\SEM6\\net java\\sprawozdanie3";
+
+            openFileDialog1.ShowDialog();
+            var file = openFileDialog1.FileName;
+            if (file != null)
+            {
+                img = new Bitmap(file);
+                img = new Bitmap(img, new Size(374, 274));
+
+                pictureBox1.Size = new Size(374, 274);
+                pictureBox1.Image = img;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap? grayscaleImage = null;
+            Bitmap? invertedImage = null;
+            Bitmap? mirrorImage = null;
+            Bitmap? thresholdingImage = null;
+
+            Parallel.Invoke(
+                () => grayscaleImage = GrayscaleFilter(img),
+                () => invertedImage = InvertColorsFilter(img),
+                () => thresholdingImage = ThresholdingFilter(img),
+                () => mirrorImage = MirrorImage(img)
+            );
+
+            pictureBox2.Size = new Size(188, 136);
+            pictureBox2.Image = invertedImage;
+
+            pictureBox3.Size = new Size(188, 136);
+            pictureBox3.Image = grayscaleImage;
+
+            pictureBox4.Size = new Size(188, 136);
+            pictureBox4.Image = thresholdingImage;
+
+            pictureBox5.Size = new Size(188, 136);
+            pictureBox5.Image = mirrorImage;
+        }
+
+        private Bitmap? ThresholdingFilter(Bitmap? originalImage)
+        {
+            Bitmap? thresholdingImage;
+            lock (locker)
+            {
+                thresholdingImage = new Bitmap(originalImage.Width, originalImage.Height);
+            }
+
+            // Najpierw zamienie na odcienie szarosci aby ustalic latwo prog dla jednego koloru a nie osobno dla skladowych RGB
+            for (int y = 0; y < thresholdingImage.Height; y++)
+            {
+                for (int x = 0; x < thresholdingImage.Width; x++)
+                {
+                    Color originalColor;
+                    lock (locker)
+                    {
+                        originalColor = originalImage.GetPixel(x, y);
+                    }
+
+                    int threshold = 125;
+                    int grayValue = (originalColor.R + originalColor.G + originalColor.B) / 3;
+
+                    if (grayValue <= threshold) thresholdingImage.SetPixel(x, y, Color.Black);
+                    else thresholdingImage.SetPixel(x, y, Color.White);
+
+                }
+            }
+
+            thresholdingImage = new Bitmap(thresholdingImage, new Size(188, 136));
+            return thresholdingImage;
+        }
+
+        private Bitmap? MirrorImage(Bitmap originalImage)
+        {
+            Bitmap? mirrorImage;
+            lock (locker)
+            {
+                mirrorImage = new Bitmap(originalImage.Width, originalImage.Height);
+            }
+
+            for (int y = 0; y < mirrorImage.Height; y++)
+            {
+                for (int x = 0; x < mirrorImage.Width; x++)
+                {
+                    lock (locker)
+                    {
+                        mirrorImage.SetPixel((mirrorImage.Width-(1+x)), y, originalImage.GetPixel(x, y));
+                    }
+                }
+            }
+            mirrorImage = new Bitmap(mirrorImage, new Size(188, 136));
+            return mirrorImage;
+        }
+
+        private Bitmap? InvertColorsFilter(Bitmap originalImage)
+        {
+            Bitmap? invertColorsImage;
+            lock (locker)
+            {
+                invertColorsImage = new Bitmap(originalImage.Width, originalImage.Height);
+            }
+
+            for (int y = 0; y < invertColorsImage.Height; y++) 
+            {
+                for (int x = 0; x < invertColorsImage.Width; x++)
+                {
+                    Color originalColor;
+                    lock (locker)
+                    {
+                        originalColor = originalImage.GetPixel(x, y);
+                    }
+
+                    int invertColorR = Math.Abs(originalColor.R - 255);
+                    int invertColorG = Math.Abs(originalColor.G - 255);
+                    int invertColorB = Math.Abs(originalColor.B - 255);
+
+                    Color invertColor = Color.FromArgb(invertColorR, invertColorG, invertColorB);
+
+                    invertColorsImage.SetPixel(x, y, invertColor);
+
+                }
+            }
+
+            invertColorsImage = new Bitmap(invertColorsImage, new Size(188, 136));
+            return invertColorsImage;
+        }
+
+        private Bitmap? GrayscaleFilter(Bitmap originalImage)
+        {
+
+            Bitmap? grayscaleImage;
+            lock (locker)
+            {
+                grayscaleImage = new Bitmap(originalImage.Width, originalImage.Height);
+            }
+            // Przetwarzam kazdy piksel mojego obrazu
+            for (int y = 0; y < grayscaleImage.Height; y++)
+            {
+                for (int x = 0; x < grayscaleImage.Width; x++)
+                {
+                    Color originalColor;
+                    lock (locker)
+                    {
+                        originalColor = originalImage.GetPixel(x, y);
+                    }
+                    int grayValue = (originalColor.R + originalColor.G + originalColor.B) / 3;
+
+                    // Tworze nowy kolor o barwie odcieni szarosci
+                    Color grayColor = Color.FromArgb(grayValue, grayValue, grayValue);
+
+                    // Ustawiam nowy kolor na obrazie
+                    grayscaleImage.SetPixel(x, y, grayColor);
+                }
+            }
+
+            // Po przetworzeniu kazdego piksela obrazu zmniejszamy jego wielkosc
+            grayscaleImage = new Bitmap(grayscaleImage, new Size(188, 136));
+            return grayscaleImage;        
         }
     }
 }
